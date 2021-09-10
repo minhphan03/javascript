6e9659a2b05a6cc0d64f69bf28b4b72d2e4d1618
Minh <66846878+minhphan03@users.noreply.github.com>
2021-09-09 19:13:09 -0700

    Create filter.js

---
+++ data-structures/filter.js
@@ -0,0 +1,10 @@
+function filteredArray(arr, elem) {
+  let newArr = [];
+  for (let i = 0 ; i< arr.length; i++)
+  if (arr[i].indexOf(elem) !== -1) {
+    newArr.push(arr[i].filter((item)=> {
+      return item !== elem;
+    }))
+  }
+  return newArr;
+}
