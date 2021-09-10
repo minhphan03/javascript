26a56a9115decb2907c1d9ac7b9c9c9f00c2de99
Minh <66846878+minhphan03@users.noreply.github.com>
2021-09-09 19:14:14 -0700

    Update filter.js

--- data-structures/filter.js
+++ data-structures/filter.js
@@ -2,6 +2,7 @@ function filteredArray(arr, elem) {
   let newArr = [];
   for (let i = 0 ; i< arr.length; i++)
   if (arr[i].indexOf(elem) !== -1) {
+    // The filter() method creates an array filled with all array elements that pass a test (provided by a function).
     newArr.push(arr[i].filter((item)=> {
       return item !== elem;
     }))
